d54394ab2dcb79a8a93a4894e9d9d7d35df200d2
Daniel F. Rincón <danyforce11@hotmail.com>
2026-05-24 15:43:00 -0500

    Se agrega vista detalle de empleados

--- personal/urls.py
+++ personal/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from .views import (
     EmpleadoListView,
     EmpleadoCreateView,
+    EmpleadoDetailView
 )
 
 urlpatterns = [
     path('', EmpleadoListView.as_view(), name='empleado_list'),
     path('nuevo/', EmpleadoCreateView.as_view(), name='empleado_create'),
+    path('<int:pk>/', EmpleadoDetailView.as_view(), name='empleado_detail'),
 ]
--- personal/views.py
+++ personal/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView
+from django.views.generic import DetailView, ListView, CreateView
 
 from .models import Empleado
 from .forms import EmpleadoForm
@@ -17,3 +17,9 @@ class EmpleadoCreateView(CreateView):
     form_class = EmpleadoForm
     template_name = 'personal/empleado_form.html'
     success_url = reverse_lazy('empleado_list')
+    
+    
+class EmpleadoDetailView(DetailView):
+    model = Empleado
+    template_name = 'personal/empleado_detail.html'
+    context_object_name = 'empleado'
